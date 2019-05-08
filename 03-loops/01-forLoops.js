3c95d940c4306e31e651a7a88ff83acf9663729d
Darrell Scrogham <dscrogham@gmail.com>
2019-05-08 13:07:27 -0400

    3rd day stuff

---
+++ 03-loops/01-forLoops.js
@@ -0,0 +1,46 @@
+//FOR LOOPS
+
+/* 
+A Loop is a programming tool that repeats a set of instructions until a specified condition is met. 
+*/
+
+
+const vacationSpots = ['Florence', 'Copenhagen', 'Isttanbul'];
+console.log(vacationSpots)[0];
+console.log(vacationSpots)[1];
+console.log(vacationSpots)[2];
+
+
+
+
+
+//Types of loops
+/*
+Do while statements
+While statements
+Labeled statement
+Break statements
+Continue statements
+For in statements
+For of satements
+
+
+ctr C - stop terminal
+
+*/
+// instiallazation/   /stop
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+//Challenge: using a for loop, count to, 2's
+
+
+for (let i = 0; i < num1; i == ) {
+    console.log(i);
+}
+
+for (let i = 0; 1 <= 20; i += 2) {
+    console.log(i)
+}
